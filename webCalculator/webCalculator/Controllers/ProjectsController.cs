6cfd5e88463d6917577c7f43dbbf5ec3dddaf2c1
Syed Nabi Hassan <syednabi932@gmail.com>
2022-11-06 19:38:38 +0500

    Initial Commit

---
+++ webCalculator/webCalculator/Controllers/ProjectsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webCalculator.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calculator(string firstNum, string secondNum)
+        {
+            double first = double.Parse(firstNum);
+            double second = double.Parse(secondNum);
+            
+            double firstsqrt = Math.Sqrt(first);
+            double secondsqrt = Math.Sqrt(second);
+
+            ViewBag.FirstNumSqrt = firstsqrt;
+            ViewBag.SecondNumSqrt = secondsqrt;
+            return View();
+        }
+    }
+}
