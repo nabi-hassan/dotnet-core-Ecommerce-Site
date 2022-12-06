460dfbc6449b332704a75f681ae84b9847f8c35a
Nabi Hassan <syednabi932@gmail.com>
2022-12-06 20:37:24 +0500

    added a new model for product data

---
+++ webCalculator/webCalculator/Models/proddtaModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace webCalculator.Models
+{
+    public class proddtaModel
+    {
+
+
+        [Key]
+        public int ProdId { get; set; }
+        public string ProdTitle { get; set; }
+        public String ProdDescription { get; set; }
+        public String ProdQuantity { get; set; }
+        public String ProdBrand { get; set; }
+        public string ProdCategory { get; set; }
+        public decimal ProdPrice { get; set; }
+    }
+}
